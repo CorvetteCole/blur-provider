e5146d10bbba7e1fdf93e1d63096317982841b16
Cole Gerdemann <corvettecole@gmail.com>
2020-09-14 17:32:57 -0400

    - minor refactoring

--- src/tracking.js
+++ src/tracking.js
@@ -18,35 +18,35 @@ let _on_window_unmanagedMap = new Map();
 let _on_actor_destroyedMap = new Map();
 let _on_actor_visibleMap = new Map();
 
-function get_window(pid){
+function get_window(pid) {
     return _windowMap.get(pid);
 }
 
-function get_actor(pid){
+function get_actor(pid) {
     return _actorMap.get(pid);
 }
 
-function set_blur_actor(pid, blurActor){
+function set_blur_actor(pid, blurActor) {
     _blurActorMap.set(pid, blurActor);
 }
 
-function get_blur_actor(pid){
+function get_blur_actor(pid) {
     return _blurActorMap.get(pid);
 }
 
-function has_window(pid){
+function has_window(pid) {
     return _windowMap.has(pid);
 }
 
-function has_actor(pid){
+function has_actor(pid) {
     return _actorMap.has(pid);
 }
 
-function has_blur_actor(pid){
+function has_blur_actor(pid) {
     return _blurActorMap.has(pid);
 }
 
-function track_new(actor, window){
+function track_new(actor, window) {
     let pid = new Date().valueOf();
     if (!_actorMap.has(pid)) {
         actor['blur_provider_pid'] = pid;
@@ -60,13 +60,13 @@ function track_new(actor, window){
     Blur.update_blur(window, pid);
 }
 
-function connect_actor_visible(pid){
+function connect_actor_visible(pid) {
     if (!_on_actor_visibleMap.has(pid)) {
         _on_actor_visibleMap.set(pid, _actorMap.get(pid).connect('notify::visible', _actor_visibility_changed))
     }
 }
 
-function remove_blur_tracking(pid){
+function remove_blur_tracking(pid) {
     _blurActorMap.delete(pid);
     _actorMap.get(pid).disconnect(_on_actor_visibleMap.get(pid));
     _on_actor_visibleMap.delete(pid);
@@ -78,18 +78,33 @@ function cleanup_windows() {
     }));
 }
 
+function cleanup_actors() {
+    _actorMap.forEach(((value, key) => {
+        _cleanup_actor(key);
+    }));
+}
+
+function print_map_info() {
+    log("map sizes, _actorMap: " + _actorMap.size + " _blurActorMap: " + _blurActorMap.size + " _windowMap: " + _windowMap.size + "_on_mutter_hint_changedMap: " + _on_mutter_hint_changedMap.size + "_on_window_unmanagedMap: " + _on_window_unmanagedMap.size + " _on_actor_destroyedMap: " + _on_actor_destroyedMap.size + " _on_actor_visibleMap: " + _on_actor_visibleMap.size);
+}
+
+function focus_changed() {
+    //log("focus_changed");
+    if (_blurActorMap.size > 0) {
+
+        let callbackId = Meta.later_add(Meta.LaterType.BEFORE_REDRAW, _fix_blur);
+        //log("callback id: " + callbackId);
+
+
+    }
+}
+
 function _cleanup_window(pid) {
     //log("cleanup_window");
     _windowMap.get(pid).disconnect(_on_mutter_hint_changedMap.get(pid));
     _windowMap.get(pid).disconnect(_on_window_unmanagedMap.get(pid));
     _on_mutter_hint_changedMap.delete(pid);
     _windowMap.delete(pid);
-}
-
-function cleanup_actors() {
-    _actorMap.forEach(((value, key) => {
-        _cleanup_actor(key);
-    }));
 }
 
 function _cleanup_actor(pid) {
@@ -106,18 +121,6 @@ function _cleanup_actor(pid) {
 
     _on_actor_destroyedMap.delete(pid);
     _actorMap.delete(pid);
-}
-
-function print_map_info() {
-    log("map sizes, _actorMap: " + _actorMap.size + " _blurActorMap: " + _blurActorMap.size + " _windowMap: " + _windowMap.size + "_on_mutter_hint_changedMap: " + _on_mutter_hint_changedMap.size + "_on_window_unmanagedMap: " + _on_window_unmanagedMap.size + " _on_actor_destroyedMap: " + _on_actor_destroyedMap.size + " _on_actor_visibleMap: " + _on_actor_visibleMap.size);
-}
-
-function focus_changed() {
-    //log("focus_changed");
-    if (_blurActorMap.size > 0) {
-        let callbackId = Meta.later_add(Meta.LaterType.BEFORE_REDRAW, _fix_blur);
-        //log("callback id: " + callbackId);
-    }
 }
 
 function _actor_visibility_changed(window_actor) {
@@ -145,7 +148,7 @@ function _mutter_hint_changed(meta_window) {
     Blur.update_blur(meta_window, meta_window.blur_provider_pid);
 }
 
-function _window_unmanaged(meta_window){
+function _window_unmanaged(meta_window) {
     //log("window_unmanaged");
     let pid = meta_window.blur_provider_pid;
     _cleanup_window(pid);
